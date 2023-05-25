1acbfe13c21e99921c6194f641151864e08deb88
Seonjin Kim <seonjin6804@gmail.com>
2023-05-26 00:21:32 +0900

    feat: change home page pos in bottom nav bar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -26,11 +26,11 @@ class _BottomNavBarState extends State<BottomNavBar> {
         });
       },
       items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
         BottomNavigationBarItem(
             icon: Icon(Icons.campaign_outlined), label: '공지'),
         BottomNavigationBarItem(
             icon: Icon(Icons.speaker_notes_outlined), label: '게시판'),
-        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
         BottomNavigationBarItem(icon: Icon(Icons.info_outline), label: '내정보'),
       ],
     );
